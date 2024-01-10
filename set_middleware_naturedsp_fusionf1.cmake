e07418e1f751947bc3ff94f4a68f4f25e3c9cfd1
Mcu_sdk Ci <mcu_sdk.ci@nxp.com>
2024-01-10 13:47:56 +0100

    Add release/2.15.0_major_rfp update
    - remove legacy manifests

--- set_middleware_naturedsp_fusionf1.cmake
+++ set_middleware_naturedsp_fusionf1.cmake
@@ -6,11 +6,19 @@ if (CONFIG_USE_middleware_naturedsp_naturedsp_fusionf1)
 
 message("middleware_naturedsp_naturedsp_fusionf1 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if((CONFIG_BOARD STREQUAL evkmimxrt595))
+if((CONFIG_BOARD STREQUAL evkmimxrt595 OR CONFIG_BOARD STREQUAL evkmimx8ulp))
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
   ${CMAKE_CURRENT_LIST_DIR}/./naturedsp/fusionf1/include
 )
+
+if(CONFIG_TOOLCHAIN STREQUAL xcc)
+  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    -Wl,--start-group
+      ${CMAKE_CURRENT_LIST_DIR}/./naturedsp/fusionf1/libnaturedsp.a
+      -Wl,--end-group
+  )
+endif()
 
 else()
 
--- set_middleware_naturedsp_hifi4.cmake
+++ set_middleware_naturedsp_hifi4.cmake
@@ -12,6 +12,14 @@ target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
   ${CMAKE_CURRENT_LIST_DIR}/./naturedsp/hifi4/include
 )
 
+if(CONFIG_TOOLCHAIN STREQUAL xcc)
+  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    -Wl,--start-group
+      ${CMAKE_CURRENT_LIST_DIR}/./naturedsp/hifi4/libnaturedsp.a
+      -Wl,--end-group
+  )
+endif()
+
 else()
 
 message(SEND_ERROR "middleware_naturedsp_naturedsp_hifi4 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
